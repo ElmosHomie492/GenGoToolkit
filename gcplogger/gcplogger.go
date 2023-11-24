0ac68c6388f60759a01d1c27db0d71a5e87fd498
ElmosHomie492 <kaleb.walker@live.com>
2023-11-24 16:01:31 -0600

    chore: initial commit

---
+++ gcplogger/gcplogger.go
@@ -0,0 +1,41 @@
+package gcplogger
+
+import (
+	"cloud.google.com/go/logging"
+	"context"
+	"fmt"
+)
+
+// LoggerClient defines the interface for a logging client
+type LoggerClient interface {
+	Logger(logID string) *logging.Logger
+	Close() error
+}
+
+// Logger represents the GCP logger
+type Logger struct {
+	client LoggerClient
+	logger *logging.Logger
+}
+
+// New initializes a new GCP logger
+func New(ctx context.Context, client LoggerClient, logName string) (*Logger, error) {
+	if client == nil {
+		return nil, fmt.Errorf("client cannot be nil")
+	}
+
+	return &Logger{
+		client: client,
+		logger: client.Logger(logName),
+	}, nil
+}
+
+// Log logs a message with the specified severity
+func (l *Logger) Log(severity logging.Severity, message string) {
+	l.logger.Log(logging.Entry{Severity: severity, Payload: message})
+}
+
+// Close closes the logger client
+func (l *Logger) Close() error {
+	return l.client.Close()
+}
